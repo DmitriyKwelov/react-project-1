0f8a6fdfe85844295b1bfa330e90da3dd76bee04
kwell <74409210+DmitriyKwelov@users.noreply.github.com>
2022-12-12 20:07:11 +0300

    lesson 85 hook useEffect

--- src/componnents/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/componnents/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus/ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatus/ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if(props.profile === null){
@@ -8,12 +9,9 @@ const ProfileInfo = (props) => {
     }
     return (
         <div>
-{/*            <div className='banner'>
-                <img className={s.banner} src="https://static.skyassets.com/contentstack/assets/bltdc2476c7b6b194dd/blt822895e5b2d9b7c7/5e381bb838beb045b856966e/Sky_Zero_image01_RGB_2500_x_950.jpg" alt="" />
-            </div>*/}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
             </div>
         </div>
     )
